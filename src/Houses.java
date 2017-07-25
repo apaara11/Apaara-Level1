853c6d261f6040ae7ec3148b16fe421289f720b6
apaara11 <league@WTS19.attlocal.net>
2017-07-25 16:22:57 -0700

    7/25/17 half

--- src/Houses.java
+++ src/Houses.java
@@ -5,56 +5,35 @@ import org.teachingextensions.logo.PenColors;
 import org.teachingextensions.logo.Tortoise;
 
 public class Houses {
-	
+
 	static Random r = new Random();
-	
+
 	public static void main(String[] args) {
-		Tortoise.setX(10);
+		Tortoise.setX(11);
 		Tortoise.setY(400);
-		
+
 		Tortoise.setPenWidth(5);
 		Tortoise.penDown();
 		Tortoise.setSpeed(10);
-		
+
 		Tortoise.show();
 
+		for (int i = 0; i < 12; i++) {
+			int roof = r.nextInt(2);
 
-		// 1. have the tortoise start in the bottom left corner
-
-		// 2. draw a house of height 100 with grass after it. This shape: |**|_
-drawHouse();
-		// 3. extract the piece of code that draws the house into a method. Draw
-		// 10 houses.
-
-		// 4. Change the method to take int height as a parameter. Draw 9 houses
-		// of different heights
-
-		// 5. Make the method take a String instead of a height.
-
-		// “small” 60
-		// “medium” 120
-		// “large” 250
-
-		// 6. Make the method take a color as well as a height. The houses are
-		// drawn in that color.
-
-		// [optional] Set the scene to night time by setting the background to
-		// black using Tortoise.getBackgroundWindow().setColor
-
-		// 7. Give the houses peaked roofs
-
-		// 8. Extract that roof code into a method “drawPointyRoof” and create a
-		// new method: “drawFlatRoof”.
-
-		// 9. make large houses have flat rooves*/
-
+			if (roof == 1) {
+				pointyHouse();
+			} else {
+				flatHouse();
+			}
+		}
 	}
-	
-	public static void drawHouse() {
 
-int height = r.nextInt(201)+50;
-Tortoise.setPenColor(PenColors.getRandomColor());		
-		
+	public static void pointyHouse() {
+
+		int height = r.nextInt(201) + 50;
+		Tortoise.setPenColor(PenColors.getRandomColor());
+
 		Tortoise.move(height);
 		Tortoise.turn(45);
 		Tortoise.move(25);
@@ -62,9 +41,26 @@ Tortoise.setPenColor(PenColors.getRandomColor());
 		Tortoise.move(25);
 		Tortoise.turn(45);
 		Tortoise.move(height);
-		
+
 		Tortoise.turn(-90);
 		Tortoise.setPenColor(Colors.Greens.DarkGreen);
 		Tortoise.move(20);
+		Tortoise.turn(-90);
+	}
+
+	public static void flatHouse() {
+		int height = r.nextInt(201) + 50;
+		Tortoise.setPenColor(PenColors.getRandomColor());
+
+		Tortoise.move(height);
+		Tortoise.turn(90);
+		Tortoise.move(25);
+		Tortoise.turn(90);
+		Tortoise.move(height);
+
+		Tortoise.turn(-90);
+		Tortoise.setPenColor(Colors.Greens.DarkGreen);
+		Tortoise.move(20);
+		Tortoise.turn(-90);
 	}
 }
