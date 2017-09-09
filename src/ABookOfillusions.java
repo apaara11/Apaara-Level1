cac511d984696171ed6070794d810b3a42c0afe3
apaara11 <league@WorthyMini-6.attlocal.net>
2017-09-09 15:56:50 -0700

    9/9/17
    I love to code.

--- src/ABookOfillusions.java
+++ src/ABookOfillusions.java
@@ -25,6 +25,9 @@ public class ABookOfillusions extends MouseAdapter {
 	
 		JFrame frame = new JFrame();
 
+		JLabel label = new JLabel();
+		
+		JLabel label2 = new JLabel();
 		
 	private void createBook() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,32 +39,55 @@ public class ABookOfillusions extends MouseAdapter {
 		frame.setSize(1111, 1111);
 		// 4. find 2 images and save them to your project’s default package
 		
-		
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
 		
+		String one = "optical-illusion-pods-michael-tompsett.jpg";
 		
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
 		
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		
+		label = loadImageFromComputer(one);
 		
 		// 8. add your JLabel to the frame
 		
+		frame.add(label);
+		
 		// 9. call the pack() method on the frame
 		
+		frame.pack();
+		
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		
+		frame.addMouseListener(this);
+	
+//---------------------------------------------------------------------
+
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
 		
+		System.out.println("CLICKED!");
 		
 		// 12. remove everything from the frame that was added earlier
 		
-		
+		frame.remove(label);
 		// 13. load a new image like before (this is more than one line of code)
+String two = "optical.png";
 		
+		JLabel label2 = new JLabel();
+		
+		label2 = loadImageFromComputer(two);
+		
+		frame.add(label2);
+		
+		frame.pack();	
 		
 		// 14. pack the frame
+		
+		
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
