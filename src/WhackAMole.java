13177665f27a074cb98c2256f4ec288b060fa431
apaara11 <league@WTS13.attlocal.net>
2018-01-27 15:53:48 -0800

    Whack a Mole (IP)

--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -1,17 +1,54 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener{
+	JButton mole= new JButton("mole");
 	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
 	
 	public static void main(String[] args) {
-	
 		WhackAMole whackamole = new WhackAMole();
-	whackamole.drawButtons();
+	int r = new Random().nextInt(24);
+	whackamole.drawButtons(r);
+	
 	
 	}
-	public void drawButtons() {
-		int r = new Random().nextInt(24);
+	public void drawButtons(int random) {
+	mole.addActionListener(this);
+		frame.setVisible(true);
+		frame.setSize(400, 200);
+		frame.add(panel);
+		for (int i = 0; i < 25; i++) {
+			if(i==random) {
+				panel.add(mole);
+			}
+			else {
+				JButton button= new JButton();
+					button.addActionListener(this);
+				panel.add(button);
+			}
+		}
+	}
+	void speak(String words) {
+		try {
+		Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+		}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()!=mole) {
+			speak("dork");
+		}
+		else {
+			
+		}
 	}
 }
