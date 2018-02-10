8e2290210c4d79741b37e991333ada3c88699845
apaara11 <league@WTS-IM-17.attlocal.net>
2018-02-10 15:57:12 -0800

    Typing Tutor

--- src/TypingTutor.java
+++ src/TypingTutor.java
@@ -41,27 +41,27 @@ public static void main(String[] args) {
 	
 tt.Frame();
 tt.generateRandomLetter();
-
-	
 	}
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
-}
+
 @Override
 public void keyTyped(KeyEvent e) {
 	// TODO Auto-generated method stub
+
 	
+
 }
 @Override
 public void keyPressed(KeyEvent e) {
 	// TODO Auto-generated method stub
+	System.out.println("You typed" + currentLetter); 
 	
 }
 @Override
 public void keyReleased(KeyEvent e) {
 	// TODO Auto-generated method stub
+	
+	currentLetter= generateRandomLetter();
+	label.setText(""+ currentLetter);
 	
 }
 }
