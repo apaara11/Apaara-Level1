17005a7b1a924c51c9173f5607661e8ed9b49a9b
ugliestcerberus <league@WorthyMini-5.attlocal.net>
2018-03-24 15:49:45 -0700

    Test

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+// this SUCKS!
+public class Test {
+JFrame frame = new JFrame();
+JLabel label = new JLabel();
+JButton b1 = new JButton();
+JButton b2 = new JButton();
+JTextField t = new JTextField();
+JPanel panel = new JPanel();
+
+public static void main(String[] args) {
+ String a = JOptionPane.showInputDialog("What grows bigger the more you take away from it?");
+
+}
+}
