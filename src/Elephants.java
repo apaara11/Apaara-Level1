a770eb927b05901e5274b479b07384a6e3dff4aa
apaara11 <league@WTS21.attlocal.net>
2018-04-14 15:58:06 -0700

    Elephants(IP)

---
+++ src/Elephants.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class Elephants {
+	private String color;
+	private int poopAmount;
+public void getColor() {
+	
+}
+public void setColor(String color) {
+	
+}
+//
+public void getPoopAmount() {
+	
+}
+public void setPoopAmount(int poopPieces) {
+	
+}
+public void nameElephant(){
+	String name= JOptionPane.showInputDialog("Name tour elephant");
+JOptionPane.showMessageDialog(null, name);
+}
+}
